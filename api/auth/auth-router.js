2eb74893aa111816d934da074bc0024a61fafcb2
Robert Jank <robert.jank@yahoo.com>
2024-11-18 10:33:15 -0600

    done

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -17,10 +17,38 @@ router.post("/register", async (req, res, next) => {
   }
 });
 router.post("/login", async (req, res, next) => {
-  res.json({ message: "login working" });
+  try {
+    const { username, password } = req.body;
+    const [user] = await User.findBy({ username });
+    if (user && bcrypt.compareSync(password, user.password)) {
+      req.session.user = user;
+      res.json({ message: `welcome back, ${user.username}` });
+    } else {
+      next({ status: 401, message: "bad credentials" });
+    }
+  } catch (err) {
+    next(err);
+  }
 });
 router.get("/logout", async (req, res, next) => {
-  res.json({ message: "ogout working" });
+  if (req.session.user) {
+    if (req.session.user) {
+      const { username } = req.session.user;
+      req.session.destroy((err) => {
+        if (err) {
+          res.json({ message: `you can never leave, ${username}` });
+        } else {
+          res.set(
+            "Set-Cookie",
+            "monkey=; SameSite=Strict; Path=/; Expires=Thu, 01 Jan 1970 00:00:00"
+          );
+          res.json({ message: `Goodbye ${username}` });
+        }
+      });
+    }
+  } else {
+    res.json({ message: "sorry, have we met?" });
+  }
 });
 
 module.exports = router;
